8f38ec7934ff416916c999f19bd74b2de78359f4
KinamKim <kimkinam1118@kaist.ac.kr>
2021-06-24 01:41:27 +0900

    Really 1 failed.

---
+++ filesys/build/filesys/inode.d
@@ -0,0 +1,15 @@
+filesys/inode.o: ../../filesys/inode.c ../../include/filesys/inode.h \
+ ../../include/lib/stdbool.h ../../include/filesys/off_t.h \
+ ../../include/lib/stdint.h ../../include/devices/disk.h \
+ ../../include/lib/inttypes.h ../../include/lib/kernel/list.h \
+ ../../include/lib/stddef.h ../../include/lib/debug.h \
+ ../../include/lib/round.h ../../include/lib/string.h \
+ ../../include/filesys/filesys.h ../../include/filesys/free-map.h \
+ ../../include/threads/malloc.h ../../include/filesys/fat.h \
+ ../../include/filesys/file.h ../../include/filesys/directory.h \
+ ../../include/filesys/page_cache.h ../../include/vm/vm.h \
+ ../../include/lib/kernel/hash.h ../../include/lib/kernel/list.h \
+ ../../include/threads/palloc.h ../../include/vm/uninit.h \
+ ../../include/vm/anon.h ../../include/vm/file.h \
+ ../../include/threads/thread.h ../../include/threads/interrupt.h \
+ ../../include/threads/synch.h
---
+++ filesys/build/tests/filesys/buffer-cache/bc-easy.d
@@ -0,0 +1,7 @@
+tests/filesys/buffer-cache/bc-easy.o: \
+ ../../tests/filesys/buffer-cache/bc-easy.c ../../include/lib/random.h \
+ ../../include/lib/stddef.h ../../include/lib/stdio.h \
+ ../../include/lib/debug.h ../../include/lib/stdarg.h \
+ ../../include/lib/stdbool.h ../../include/lib/stdint.h \
+ ../../include/lib/kernel/stdio.h ../../include/lib/user/syscall.h \
+ ../../tests/lib.h ../../tests/main.h
